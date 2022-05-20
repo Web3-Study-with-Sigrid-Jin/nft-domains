4022cea2676d9e46b55a238c619b278089e5343a
Jin Hyung Park "Sigrid Jin <sigrid.jinhyung@gmail.com>
2022-05-20 11:46:40 +0900

    feat: checkIfWalletIsConnected

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 
@@ -7,6 +7,34 @@ const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+	
+	const checkIfWalletIsConnected = () => {
+		// window.ethereum 은 메타마스크를 호출
+		const { ethereum } = window;
+
+		if (!ethereum) {
+			console.log("메타마스크가 없어요")
+			return;
+		}
+
+		console.log("메타마스크 ethereum object 에요", ethereum);
+		return;
+	}
+
+	// 메타마스크가 연결되어 있지 않을 때 render 하는 container 에요
+	const renderNotConnectedContainer = () => (
+		<div className="connect-wallet-container">
+		  <img src="https://media.giphy.com/media/3ohhwytHcusSCXXOUg/giphy.gif" alt="Ninja gif" />
+		  <button className="cta-button connect-wallet-button">
+			Connect Wallet
+		  </button>
+		</div>
+		);
+	
+	  // useEffect 라서 페이지가 로딩될 때 아래 함수가 자동으로 실행되어요
+	  useEffect(() => {
+		checkIfWalletIsConnected();
+	  }, [])
 
   return (
 		<div className="App">
@@ -20,6 +48,8 @@ const App = () => {
             </div>
 					</header>
 				</div>
+
+				{renderNotConnectedContainer()}
 
         <div className="footer-container">
 					<img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
